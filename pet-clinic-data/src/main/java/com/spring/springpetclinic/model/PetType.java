641c675037b750e235d29d370320a50d68318b72
thaheer-shaik <thahir074@gmail.com>
2021-07-26 20:51:31 +0530

    refactoring to multimodule maven project. Closes #1.

--- pet-clinic-data/src/main/java/com/spring/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/spring/springpetclinic/model/PetType.java
@@ -12,3 +12,4 @@ public class PetType
         this.name = name;
     }
 }
+
--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.springpetclinic;
+package springpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
