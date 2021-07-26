27392ff9e13c77c806c23d60433f2aa7895a5d2f
thaheer-shaik <thahir074@gmail.com>
2021-07-27 01:58:06 +0530

    Added id to model objects closes #10

--- pet-clinic-data/src/main/java/com/spring/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/spring/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.spring.springpetclinic.model;
 
-public class Person
+public class Person extends BaseEntity
 {
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/spring/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/spring/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.spring.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet
+public class Pet extends BaseEntity
 {
     private PetType petType;
     private Owner owner;
