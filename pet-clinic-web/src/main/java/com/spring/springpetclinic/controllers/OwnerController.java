1c3979914ce7e633aa7b071aefcccbe39b14e118
thaheer-shaik <thahir074@gmail.com>
2021-08-03 22:24:03 +0530

    Created Owner Index page and Controller. closes #14

---
+++ pet-clinic-web/src/main/java/com/spring/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.spring.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController
+{
+    @RequestMapping("/owner")
+    public String getVet()
+    {        return "/owner/index";
+    }
+}
