0db544f4c586488e19d05fb738f75403c44957a2
thaheer-shaik <thahir074@gmail.com>
2021-08-05 14:20:04 +0530

    added the map implementation to manage the ID preperty. closes #24

--- pet-clinic-data/src/main/java/com/spring/springpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/spring/springpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.spring.springpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.spring.springpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID>
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long>
 {
-    protected Map<ID,T> map=new HashMap<>();
+    protected Map<Long,T> map=new HashMap<>();
 
     Set<T> findAll()
     {
@@ -19,9 +18,20 @@ public abstract class AbstractMapService<T,ID>
         return map.get(id);
     }
 
-    T save(ID id,T object)
+    T save(T object)
     {
-        map.put(id,object);
+        if(object!=null)
+        {
+            if(object.getId()==null)
+            {
+                object.setId(getNextId());
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Object cannot be null");
+        }
+        map.put(object.getId(),object);
 
         return object;
     }
@@ -34,6 +44,19 @@ public abstract class AbstractMapService<T,ID>
     {
         map.entrySet().removeIf(entry ->entry.getValue().equals(object));
     }
+    private Long getNextId()
+    {
+        Long nextId=null;
 
+        try
+        {
+            nextId= Collections.max(map.keySet())+1;
+        }
+        catch(NoSuchElementException e)
+        {
+            nextId=1L;
+        }
+        return nextId;
+    }
 
 }
